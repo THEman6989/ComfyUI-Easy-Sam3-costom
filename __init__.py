db41492b1f143c7a4fe30feeb675a59033d98517
THEman6989 <129108560+THEman6989@users.noreply.github.com>
2026-02-18 01:37:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@ class Sam3Extension(ComfyExtension):
             Sam3GetObjectMask,
             StringToBBox,
             FramesEditor,
+            Sam3VideoSegmentation_v2,
         ]
 
 async def comfy_entrypoint() -> Sam3Extension:
